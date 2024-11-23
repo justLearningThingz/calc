104e67cc82934052ef9832543a88df05726a9cb3
ludic <notetakingforeggs@gmail.com>
2024-11-23 17:46:39 +0000

    Fix: division by zero

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const numCmdButtons = document.querySelectorAll(".num,.cmd");
 const input = document.querySelector("input");
 const operationArray = [];
+const equals = document.querySelector(".equals");
+const ac = document.querySelector(".ac")
 
 numCmdButtons.forEach((button)=>{
         button.addEventListener("click", function (e){
@@ -9,15 +11,31 @@ numCmdButtons.forEach((button)=>{
         });
 });
 
+equals.addEventListener("click", ()=>{
+    let result = operate(operationArray[0], operationArray[1], operationArray[2]);
+    input.value = result;
+    operationArray.length = 0;
+    if(!+result == NaN) operationArray.push(result);
+});
+ac.addEventListener("click", ()=>{
+    input.value = "";
+    operationArray.length = 0;
 
-
+})
 
 function operate (a, operator, b){
+    if(operator == "/" && b == "0"){
+
+      return "division by zero";
+    }
     
+    a = +a;
+    b = +b;
+
     switch(operator){
         case("+"):return a+b;
         case("-"):return a-b;
-        case("*"):return a*b;
+        case("X"):return a*b;
         case("/"):return a/b;
         default:{
             console.log("illegal operator");
